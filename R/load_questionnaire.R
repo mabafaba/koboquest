2110d52ce689c6c12a5134d12f13c91dabadddc9
Martin B <barner.mar@gmail.com>
2018-10-17 09:59:29 +0200

    checks

--- R/load_questionnaire.R
+++ R/load_questionnaire.R
@@ -1,6 +1,6 @@
 
 # questionnaire_is_loaded <- FALSE
-# 
+#
 
 #' load_questionnaire
 #' @param data data frame containing the data matching the questionnaire to be loaded.
@@ -14,7 +14,7 @@
 load_questionnaire<-function(data,
                              questions.file,
                              choices.file,
-                             choices.label.column.to.use="label..English"){
+                             choices.label.column.to.use=NULL){
   # generic function to remove non-data from vectors
   hasdata<-function (x, return.index = F) {
     index <- which(!is.null(x) & !is.na(x) & x != "" & !is.infinite(x))
@@ -24,17 +24,18 @@ load_questionnaire<-function(data,
     }
     return(value)
   }
+
   # generic function to replace values in a vector based on a lookup table
   replace_with_lookup_table<-function(x,y){
     x2 <- y[match(x, y[,1]),2]
     dim(x2) <- dim(x)
     x2
   }
-  
+
   # load files
   questions <- read.csv.auto.sep(questions.file,stringsAsFactors = F, header = T)
   choices <- read.csv(choices.file, stringsAsFactors = F, header = T)
-  
+
   # harmonise data column references
   names(questions) <- to_alphanumeric_lowercase(names(questions))
   names(choices) <- to_alphanumeric_lowercase(names(choices))
@@ -42,10 +43,10 @@ load_questionnaire<-function(data,
 
 
   # choices$name <- gsub("_", ".", choices$name) # UGANDA
-  
+
 
   choices.label.column.to.use <- to_alphanumeric_lowercase(choices.label.column.to.use)
-  
+
   # sanitise
   insure.string.is.column.header(questions, "type")
   insure.string.is.column.header(questions, "name")
@@ -53,29 +54,29 @@ load_questionnaire<-function(data,
   insure.string.is.column.header(choices, "list_name")
   questions$name <- to_alphanumeric_lowercase(questions$name)
 
-  
+
   # # ####UGA ONLY
   # questions$name <- gsub("_", ".", questions$name)
   # choices$name <- gsub("_", ".", choices$name)
   # names(data) <- gsub("_", ".", names(data))
   # questions$relevant <- gsub("_", ".", questions$relevant) %>% to_alphanumeric_lowercase
-  
+
   # # UGANDA
 
   begin_gr <- grep(paste(c("begin_group","begin group"), collapse = "|"), questions$type, ignore.case = T)
   end_gr <- grep(paste(c("end_group","end group"), collapse = "|"), questions$type, ignore.case = T)
   number_of_questions <- (length(questions$name) - length(begin_gr) - length(end_gr))
-  
+
   questions$relevant<-add_group_conditions_to_question_conditions(questions)
   # get data column names
   data_colnames<-names(data)
-  
+
   # this changes the questionnaire questions and choices to fit the data columns,
   # with empty entries for data columns that don't appear in the questionnaire.
   if((sum(!is.na(match(data_colnames, questions$name)))/number_of_questions) < 0.1) {
     stop("The question names (questionnaire) and data column names (data) don't seem to match. please make sure the two columns are harmonized")
   }
-  
+
       questions <- questions[match(data_colnames, questions$name),]
 
         choices_per_data_column<-questions$type %>% as.character %>% strsplit(" ") %>% lapply(unlist)%>% lapply(function(x){
@@ -83,8 +84,8 @@ load_questionnaire<-function(data,
         x %>% lapply(function(y){
         # grep(y,choices[["list_name"]],value=F)
         # match (full word only)
-        grep(paste0(" ",y," "),paste0(" ",choices[["list_name"]]," "),value=F,fixed = T)
-          
+        grep(paste0(" ",y," "),paste0(" ",choices[["list.name"]]," "),value=F,fixed = T)
+
       }
       ) %>% unlist
     }) %>% lapply(hasdata) %>% lapply(function(x){
@@ -108,25 +109,25 @@ load_questionnaire<-function(data,
       # fix those that were not found to go back to original NA
       labels[is.na(labels)]<-responses[is.na(labels)]
       return(labels)
-      
+
       }
      return(responses)
    }
    question_get_question_label<<-function(variable.names){
      variable.names<-as.character(variable.names)
-     
+
      labelcol<-grep("label",names(questions))[1]
      questionnaire_rows<-match(variable.names,questions[,"name"])
      questionnaire_rows[is.na(variable.names)]<-NA
      labels<-questionnaire_rows
      labels[!is.na(questionnaire_rows)]<-questions[questionnaire_rows[!is.na(questionnaire_rows)],labelcol]
      labels[is.na(labels)]<-variable.names[is.na(labels)]
-     
+
      labels[gsub("[[:space:]]", "", labels)==""| is.na(variable.names)]<-variable.names[gsub("[[:space:]]", "", labels)==""| is.na(variable.names)]
      return(labels)
       }
-   
-    
+
+
 
     question_is_numeric <<- function(question.name){
       if(is.null(question.name)){return(FALSE)}
@@ -165,7 +166,7 @@ load_questionnaire<-function(data,
       if(question.name==""){return(FALSE)}
       return(question_is_select_one(question.name) | question_is_select_multiple(question.name))
     }
-    
+
     question_in_questionnaire <<- function(question.name){
       if(sum(question.name %in% questions$name) > 0){
         return(TRUE)}
@@ -177,75 +178,75 @@ load_questionnaire<-function(data,
       condition<-questions$relevant[qid[1]]
       question_is_skipped_apply_condition_to_data(data,condition)
     }
-    
+
 
     is_questionnaire_loaded<<-function(){return(TRUE)
     }
-    
-    
-    
+
+
+
     # message(blue("load_questionnaire() activated the following functions:
-    # 
-    # 
+    #
+    #
     #          identifying data type:
-    # 
+    #
     #          question_is_numeric()
     #          question_is_categorical()
     #          question_is_categorical()
     #          question_is_select_one()
     #          question_is_select_multiple()
     #          question_variable_type()
-    # 
+    #
     #          labels:
     #          question_get_choice_labels()
     #          question_get_question_label()
-    # 
+    #
     #          skiplogic:
     #          question_is_skipped()"))
     questionnaire_is_loaded <- TRUE
-    
+
     is_questionnaire_loaded<-function(){return(TRUE)}
 
-    
+
     # select_multiple_names<-names(data)[question_is_select_multiple(names(data))]
     # sm_choice_varnames<-lapply(select_multiple_names,function(sm_varname){
     #   paste(sm_varname,choices_per_data_column[[sm_varname]]$name,sep=".")
     # })
     # questions$type[(questions$name %in% sm_choice_varnames) & is.na(questions$type)]<-"sm_choice"
-    
+
     question_is_sm_choice<<-function(question.name){
       if(is.null(question.name)){return(FALSE)}
       if(is.na(question.name)){return(FALSE)}
       if(question.name==""){return(FALSE)}
-      
+
       var.name.split<-strsplit(question.name,"\\.")
       question.name.sans.choice<-paste0(var.name.split[[1]][-length(var.name.split[[1]])],collapse=".")
       choice.name<-var.name.split[[1]][length(var.name.split[[1]])]
-      
-      if(!question_is_select_multiple(question.name.sans.choice)){return(FALSE)}      
-      
+
+      if(!question_is_select_multiple(question.name.sans.choice)){return(FALSE)}
+
       if(choice.name %in% choices_per_data_column[[question.name.sans.choice]]$name){
         return(TRUE)
       }
       return(FALSE)
     }
-    
+
     return(c(list(questions=questions,choices=choices,choices_per_variable=choices_per_data_column), data))
 
     }
 
 
-    
+
 question_get_choice_labels<-function(responses,variable.name){
       stop("you must successfully run load_questionnaire() first")
 
     }
 
-    
+
     question_get_question_label<-function(variable.name){
       stop("you must successfully run load_questionnaire() first")
     }
-    
+
     question_is_numeric<-function(question.name){
       stop("you must successfully run load_questionnaire() first")
     }
@@ -262,7 +263,7 @@ question_get_choice_labels<-function(responses,variable.name){
 
     question_is_sm_choice<-function(question.name){
       stop("you must successfully run load_questionnaire() first")
-      
+
     }
     question_is_categorical<-function(question.name){
       stop("you must successfully run load_questionnaire() first")
@@ -270,10 +271,10 @@ question_get_choice_labels<-function(responses,variable.name){
 
     is_questionnaire_loaded<-function(){return(FALSE)
     }
-    
+
     question_in_questionnaire <- function(question.name){return(FALSE)
     }
-    
+
     question_is_skipped<-function(data, variable.name){
       stop("you must successfully run load_questionnaire() first")
     }
@@ -292,7 +293,7 @@ question_variable_type_from_questionnaire <- function(variables){
       if(question_is_select_multiple(x)){return("select_multiple")}
       if(question_is_select_one(x)){return("select_one")}
       if(question_is_numeric(x)){return("numeric")}
-      
+
       return(NA)
       })
     )
@@ -309,15 +310,15 @@ question_variable_type_from_questionnaire <- function(variables){
 add_group_conditions_to_question_conditions<-function(questions){
   group_conditions<-NULL
   conditions<-c()
-  
+
   begin_gr <- grep(paste(c("begin_group","begin group"), collapse = "|"), questions$type, ignore.case = T)
   end_gr <- grep(paste(c("end_group","end group"), collapse = "|"), questions$type, ignore.case = T)
-  
-  
+
+
   for(i in 1:nrow(questions)){
     is_group_start<-(i %in% as.numeric(begin_gr))
     is_group_end<-(i %in% as.numeric(end_gr))
-    
+
     if(is_group_start){
 
       group_conditions<-c(group_conditions,questions$relevant[i])
@@ -330,21 +331,21 @@ add_group_conditions_to_question_conditions<-function(questions){
     if(!is_group_end & !is_group_start){
       condition_that_only_applies_to_this_question<-questions$relevant[i]
     }
-    
+
     all_condition_for_this_q<-c(group_conditions,condition_that_only_applies_to_this_question)
-    
+
     if(all(all_condition_for_this_q=="")){
         all_condition_for_this_q<-""
       }else{
         all_condition_for_this_q<-paste("(",all_condition_for_this_q[all_condition_for_this_q!=""],")")
       }
-    
+
     all_condition_for_this_q_combined<-paste(all_condition_for_this_q,collapse=" and ")
-    conditions<-c(conditions,all_condition_for_this_q_combined)  
+    conditions<-c(conditions,all_condition_for_this_q_combined)
   }
-  
-  
-  
+
+
+
   conditions
 }
 
