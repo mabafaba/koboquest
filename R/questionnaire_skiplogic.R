fed4b5dff9dd6deca9073163f355a3264be4f34e
Martin B <barner.mar@gmail.com>
2019-03-27 18:51:22 +0100

    more secure <= and >= regex handling in skiplogic translation to R

--- R/questionnaire_skiplogic.R
+++ R/questionnaire_skiplogic.R
@@ -28,6 +28,7 @@ question_is_skipped_apply_condition_to_data<-function(data,condition){
   condition_rexpression<-rify_condition(condition)
 
   # run the r expression:
+  not<-function(x){!x}
   relevant<-with(data,{
   relevant<-tryCatch({
     relevant<-condition_rexpression %>% parse(text = .) %>% eval
@@ -61,6 +62,7 @@ rify_condition<-function(x){
 
 
 
+
 rify_varnames_in_string<-function(x){
   # turns "${varname}" into varname by removing any "{", "}",  "$"
     x<-gsub("[\\{\\$\\}]","",x)
@@ -70,10 +72,17 @@ rify_logical_operators<-function(x){
   # "and" , "or" -> "&" , "|"
   #  "=" -> "=="
   # both trying to avoid false matches (don't do "==" -> "==="),  "band" -> "b&" etc.
-  single_equal_sign_pattern<-"([^=])[=]([^=])"
-  x<-gsub("[[:space:]]and[[:space:]]","&",x) %>% gsub("^and[[:space:]]","&",.) %>% gsub("[[:space:]]and$","&",.) %>% gsub("^and$","&",.) %>%
-    gsub("[[:space:]]or[[:space:]]","|",.) %>% gsub("^or[[:space:]]","|",.) %>% gsub("[[:space:]]or$","|",.) %>% gsub("^or$","|",.)
-  x <-gsub(single_equal_sign_pattern,"\\1==\\2",x)
+  x<-gsub("[[:space:]]and[[:space:]]","&",x) %>% # "and" with spaces around
+     gsub("^and[[:space:]]","&",.) %>% # "and" at start of string
+     gsub("[[:space:]]and$","&",.) %>% # "and" at end of string
+     gsub("^and$","&",.) %>% # lonely "and"
+     gsub("[[:space:]]or[[:space:]]","|",.) %>% # "or" with spaces around
+     gsub("^or[[:space:]]","|",.) %>% # "or" at start of string
+     gsub("[[:space:]]or$","|",.) %>% # "or at end of string
+    gsub("^or$","|",.) # lonely "or"
+
+  single_equal_sign_pattern<-"([^=<>])[=]([^=])"
+  x <-gsub(single_equal_sign_pattern,"\\1==\\2",x) # equal sign that doesn't have "=" before or after, and doesnt have "<" or ">" before.
   x
 }
 
